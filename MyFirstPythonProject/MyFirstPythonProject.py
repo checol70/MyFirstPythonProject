64ee6b7d4582f289b5867b8238ef6b8a52bc8fc0
Sheckerol <checol70@gmail.com>
2018-12-06 20:55:35 -0700

    Add project files.

---
+++ MyFirstPythonProject/MyFirstPythonProject.py
@@ -0,0 +1,12 @@
+welcome = input("Enter a welcome ")
+#1+2
+#Colton is amazing since he can comment python
+aBoolean = True
+'''if
+elif
+else:'''
+"""hello"""
+def say_hello():
+    print("Hello")
+say_hello()
+print(welcome)
